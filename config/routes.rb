2f73dd5142c30ada7aa89ff5ae76d63eae19ea9e
Carson Meiling <carmc93@gmail.com>
2020-03-04 09:12:50 -0700

    added songs and artist controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'songs/index'
+  get 'songs/new'
+  get 'songs/show'
+  get 'songs/edit'
+  get 'artists/index'
+  get 'artists/new'
+  get 'artists/show'
+  get 'artists/edit'
   root "billboards#index"
 
   resources :billboards
