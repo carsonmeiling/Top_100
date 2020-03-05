f4b843f8938f37b056ed2b8bc10329d334c96e96
Carson Meiling <carmc93@gmail.com>
2020-03-04 18:01:15 -0700

    working on billboards create and update

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -11,7 +11,32 @@ class BillboardsController < ApplicationController
     @billboard = Billboard.find(params[:id])
   end
 
+  def update
+    @billboard = Billboard.find(params[:id])
+    if @billboard.update(billboard_params)
+      redirect_to (@billboard)
+    else
+      redirect_to :edit
+    end
+  end
+
   def new
     @billboard = Billboard.new
+  end
+
+  def create
+    @billboard = Billboard.find(params[:id])
+    if @billboard.save
+      redirect_to (@billboard)
+    else
+      redirect_to :new
+    end
+  end
+
+
+  private
+
+  def billboard_params
+    params.require(:billboard_params).permit(:title)
   end
 end
