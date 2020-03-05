a579c726d56f71946426c75b93aacb7d7dfd8ff0
Carson Meiling <carmc93@gmail.com>
2020-03-05 16:01:01 -0700

    added song controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
 class SongsController < ApplicationController
+  before_action :set_artist
+  before_action :set_song, only [:show, :edit, :update :destroy]
+
   def index
     @songs = Song.all
   end
@@ -7,10 +10,43 @@ class SongsController < ApplicationController
     @songs = Song.new
   end
 
+  def create
+    @song = Songs.new(song_params)
+    if @song.save
+      redirect_to [@artist, @song]
+    else
+      render :new
+    end
+  end
+
   def show
-    @songs = Songs.find(params[:billboard_id])
   end
 
   def edit
   end
+
+  def update
+    @song = Song.update(song_params)
+    if @song.update
+      redirect_to [@artist, @song]
+    else
+      render :edit
+  end
+
+
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:artist_id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name, :length)
+  end
+
+  def set_song
+    @songs = Songs.find(params[:artist_id])
+  end
+
 end
